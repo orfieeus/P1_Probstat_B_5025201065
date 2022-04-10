088b20749296634aea1cb01b813e2926bbc7467e
orfieeus <103368693+orfieeus@users.noreply.github.com>
2022-04-10 20:32:25 +0700

    Add files via upload

---
+++ 4.R
@@ -0,0 +1,15 @@
+#4.a
+dof = 10
+x = 2
+dchisq(2, 10)
+
+#4.b
+set.seed(0)
+dof = 10
+x = 100
+y = rchisq(x, dof)
+hist(y)
+
+#4.c
+#Mean dari Distribusi Chisquare adalah v = 10
+#Varians dari Distribusi Chisquare adalah 2v = 20
